c46b246c3168163fc50cbc1909c6b8febfc3c4db
Oskar Eriksson <oskaerik@gmail.com>
2019-03-21 11:02:00 +0100

    Update direction

--- gym_curve/envs/curve_env.py
+++ gym_curve/envs/curve_env.py
@@ -8,9 +8,9 @@ from enum import IntEnum
 from stable_baselines import PPO2
 
 SHAPE = (36, 36)
-CHARACTERS = (' ', '#', 'S', 'A', '@')
+CHARACTERS = (' ', '#', 'S', 'A')
 SLEEP = 0.1
-AGENTS = 6
+AGENTS = 3
 WIN = 1000
 
 class Action(IntEnum):
@@ -104,12 +104,18 @@ class CurveEnv(gym.Env):
               f'\tNo-op: {self._move_count[Action.noop]}')
 
         layers = [np.copy(layer) for layer in self._layers[0]]
-        state = [factor*layer for factor, layer in zip([1, -1, 3, 3], layers)]
+        state = [factor*layer for factor, layer in zip([1, -1, 0, 3], layers)]
         state = np.sum(state, axis=0, dtype=np.uint8)
         for r in range(SHAPE[0]):
             for c in range(SHAPE[1]):
                 print(CHARACTERS[state[r, c]], end=' ')
             print()
+
+        # print('\n--- Direction ---')
+        # for r in range(SHAPE[0]):
+        #     for c in range(SHAPE[1]):
+        #         print('#' if self._layers[0][2][r, c] else ' ', end=' ')
+        #     print()
         time.sleep(SLEEP)
 
 
@@ -171,7 +177,7 @@ class CurveEnv(gym.Env):
 
 
     def _get_states(self):
-        # layers = (wall_layer, self_layer, past_self_layer, agents_layer)
+        # layers = (wall_layer, self_layer, direction_layer, agents_layer)
         wall_layer, _, _, agents_layer = self._layers[0]
 
         # Update agent layer
@@ -180,13 +186,30 @@ class CurveEnv(gym.Env):
             if agent is not None:
                 agents_layer[agent[0]] = 1
 
-        # Update self layers
-        for i, (_, self_layer, past_self_layer, _) in enumerate(self._layers):
-            past_self_layer[past_self_layer == 1] = 0
-            past_self_layer[self_layer == 1] = 1
+        # Update self layer and direction layer
+        for i, (_, self_layer, direction_layer, _) in enumerate(self._layers):
             self_layer[self_layer == 1] = 0
+            direction_layer[direction_layer == 1] = 0
             if self._state['agents'][i] is not None:
                 self_layer[self._state['agents'][i][0]] = 1
 
-        return [(wall_layer, self_layer, past_self_layer, agents_layer)
-                for _, self_layer, past_self_layer, _ in self._layers]
+                _, direction = self._state['agents'][i]
+                if direction == Direction.up:
+                    for r in range(SHAPE[0]//2):
+                        for c in range(SHAPE[1]):
+                            direction_layer[r, c] = 1
+                elif direction == Direction.down:
+                    for r in range(SHAPE[0]//2, SHAPE[0]):
+                        for c in range(SHAPE[1]):
+                            direction_layer[r, c] = 1
+                elif direction == Direction.left:
+                    for r in range(SHAPE[0]):
+                        for c in range(SHAPE[1]//2):
+                            direction_layer[r, c] = 1
+                elif direction == Direction.right:
+                    for r in range(SHAPE[0]):
+                        for c in range(SHAPE[1]//2, SHAPE[1]):
+                            direction_layer[r, c] = 1
+
+        return [(wall_layer, self_layer, direction_layer, agents_layer)
+                for _, self_layer, direction_layer, _ in self._layers]
